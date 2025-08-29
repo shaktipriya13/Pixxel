be55f7fb2a6a516de6b4543dc9f516e2c00d432e
shaktipriya13 <priyashakti68@gmail.com>
2025-08-29 21:35:19 +0530

    feat: add Radix UI components and theme provider
    - Added new dependencies for Radix UI components including dropdown menu, select, slider, switch, tabs, and others.
    - Implemented a ThemeProvider component to manage theme context using next-themes.
    - Created UI components for Button, DropdownMenu, Input, Select, Slider, Switch, Tabs, and Toaster with appropriate styling and functionality.
    - Enhanced accessibility and usability features across components.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter=Inter({subsets:["latin"]}); 
 
 export const metadata = {
   title: "Editra",
@@ -18,11 +11,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
