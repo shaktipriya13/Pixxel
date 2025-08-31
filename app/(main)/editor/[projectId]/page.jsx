030cb13d1d4a5db864228ae53e085c2de3b669b8
shaktipriya13 <priyashakti68@gmail.com>
2025-08-31 15:37:33 +0530

    feat: add Dashboard and Editor components for main application structure

---
+++ app/(main)/editor/[projectId]/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+const Editor = async() => {
+    const {projectId}=await useParams();
+  return (
+    <div>Editor: {projectId}</div>
+  )
+}
+
+export default Editor
